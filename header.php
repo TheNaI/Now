4be2554737de6f507448b3f65b30d0f985af36c0
Sutinai Jaradwilaswanit <The-nai@hotmail.com>
2016-08-14 20:12:58 +0700

    Header

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="css/main.css">
@@ -19,42 +20,31 @@
     <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 </head>
 <body>
-    <nav class="navbar navbar-default" role="navigation">
+    <nav class="now-navbar navbar navbar-default" role="navigation">
     	<!-- Brand and toggle get grouped for better mobile display -->
     	<div class="navbar-header">
-    		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-    			<span class="sr-only">Toggle navigation</span>
-    			<span class="icon-bar"></span>
-    			<span class="icon-bar"></span>
-    			<span class="icon-bar"></span>
-    		</button>
-    		<a class="navbar-brand" href="#">Home</a>
+    		<a class="navbar-brand" href="#"><i class="nav-logo"></i></a>
     	</div>
-    
-    	<!-- Collect the nav links, forms, and other content for toggling -->
-    	<div class="collapse navbar-collapse navbar-ex1-collapse">
-    		<ul class="nav navbar-nav">
-    			<li class="active"><a href="#">Link</a></li>
-    			<li><a href="#">Link</a></li>
-    		</ul>
-    		<form class="navbar-form navbar-left" role="search">
-    			<div class="form-group">
-    				<input type="text" class="form-control" placeholder="Search">
-    			</div>
-    			<button type="submit" class="btn btn-default">Submit</button>
-    		</form>
-    		<ul class="nav navbar-nav navbar-right">
-    			<li><a href="#">Link</a></li>
-    			<li class="dropdown">
-    				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-    				<ul class="dropdown-menu">
-    					<li><a href="#">Action</a></li>
-    					<li><a href="#">Another action</a></li>
-    					<li><a href="#">Something else here</a></li>
-    					<li><a href="#">Separated link</a></li>
-    				</ul>
-    			</li>
-    		</ul>
-    	</div><!-- /.navbar-collapse -->
+        <div class="now-nav-menu">
+            <a data-toggle="modal" href="#modal-menu" class="menu-btn">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </a>
+        </div>
     </nav>
-    
+<div class="modal fade" id="modal-menu">
+	<div class="modal-dialog">
+		<div class="logo-menu">
+			<div class="menu-left">
+				<i class="nav-logo black"></i>
+			</div>
+			<div class="menu-right">
+				<a  class="close" data-dismiss="modal" aria-hidden="true">
+					MENU <i class="close-icon" aria-hidden="true"></i>
+				</a>
+			</div>
+		</div>
+		<div class="menu">
+			
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
--- index.php
+++ index.php
@@ -4,7 +4,20 @@ include('header.php');
 
 ?>
 
-body
+<div class="container-body" style="background-image: url('img/bg_homepage.png');">
+    <div class="banner">
+        <img src="img/bannert_homepage.jpg" alt="">
+    </div>
+    <div class="under-banner">
+        <div class="container">
+            <p class="text-banner">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nunc sapien, <br>
+                varius quis orci. Integer dignissim, purus eget sollicitudin bibendum, nulla<br>
+                pharetra luctus sapien ante sed nibh. Aliquam erat volutpat. Ut blandit cursus<br>
+                turpis risus, condimentum eu diam a, posuere sollicitudin justo. Ut venenatis.            </p>
+        </div>
+    </div>
+</div>
 
 <?php
 
